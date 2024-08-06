12c80ee1dc92a8fc71d533045902965c9a3732b9
Luiscv1719 <luiscv1719@gmail.com>
2024-08-06 01:14:56 -0500

    creacion de proyecto

---
+++ CursoC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.- HOLA MUNDO*/
+            Console.WriteLine("hola mundo");
+            /*2.-VARIABLES Y CONSTANTES */
+
+            /*3.-OPERADORES ARITMETICOS */
+
+            /*4.-CONDICIONALES(IF, ELSE, ELSE-IF) */
+
+            /*5.-CONDICIONAL SWTCH - CASE */
+
+            /*6.-METODOS */
+
+            /*7.-BUCLE FOR */
+
+            /*8.- BUCLE WHILE */
+
+            /*9.- BUCLE DO WHILE */
+
+            /*10.- ARRAYS */
+
+            /*11.- POO */
+
+
+
+
+
+
+        }
+    }
+}
