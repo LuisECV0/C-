642dfb8c8588baf11f3a101f253059ddaa144547
Luiscv1719 <luiscv1719@gmail.com>
2024-08-06 23:24:35 -0500

    variables

--- CursoC/Program.cs
+++ CursoC/Program.cs
@@ -11,8 +11,23 @@ namespace CursoC
         static void Main(string[] args)
         {
             /*1.- HOLA MUNDO*/
+
             Console.WriteLine("hola mundo");
+
             /*2.-VARIABLES Y CONSTANTES */
+
+            int numeroEntero;
+            numeroEntero = 0;
+            double numeroDecimal;
+            numeroDecimal = 10.5;
+            Console.WriteLine(numeroEntero + numeroDecimal);
+            
+            String texto = "texto de prueba";
+            Console.WriteLine(texto);
+
+            const int CONSTANTE = 100;
+          
+            Console.WriteLine(CONSTANTE);
 
             /*3.-OPERADORES ARITMETICOS */
 
